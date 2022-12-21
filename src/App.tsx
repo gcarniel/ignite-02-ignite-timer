56eb799cc151938f4fc6d67a447180a99985c164
Gabriel Carniel <gabrielcarniel@Gabriel-MacBook.local>
2022-12-21 11:32:28 -0300

    configurando projeto e estilos

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
+import { Button } from './components/Button';
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
